f3c60fe8d9ab3035ffa88ad927e8e12edb2eb3ea
99ojas@gmail.com <99ojas@gmail.com>
2022-02-21 22:41:59 +0530

    Starting programs

--- Calculator+.py
+++ Calculator+.py
@@ -1,3 +1,5 @@
+# this is the modern day calculator but sed it does not have GUI :(
+
 a=int(input("Enter first number:"))
 b=int(input("Enter second number:"))
 c=input("Enter your choice of operator:")
@@ -11,4 +13,16 @@ elif c=='/':
     print("After seprating these numbers we get" ,a/b)
 elif c=='Ojas is pro':
     print("Correct password given the access")
+elif c=='%':
+    print("The answer is-" ,a%b)
+elif c=='**':
+    print("the answer is-" ,a**b)
+elif c=='//':
+    print("The answer is-" ,a//b)
+elif c=='birla bhai op':
+    print("sahi bola")
+
+
+
+
 
--- additon calculator.py
+++
@@ -1,6 +0,0 @@
-a=int(input("Enter first number:"))
-b=int(input("Enter second number:"))
-results=a+b
-print (results)
-
-print("Thank you for doing your calculatuion:)")

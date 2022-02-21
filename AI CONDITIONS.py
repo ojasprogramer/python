1098ac38c5d680682c361fe4d01291b6601daaa1
99ojas@gmail.com <99ojas@gmail.com>
2022-02-21 23:13:38 +0530

    AI

---
+++ AI CONDITIONS.py
@@ -0,0 +1,55 @@
+# Generating ideas for this program
+
+from email.errors import InvalidBase64PaddingDefect
+
+
+a=input("Hello sir/madam What is your name?:")
+b=input("What is your age?:")
+c=input("Which standard in you are?:")
+
+# All conditions of 'a'
+if a=='Ojas':
+    print("Hello owner.")
+elif a=='Ojasvi':
+    print("Why you are here?")
+elif a=='mehu':
+    print("Bad girl")
+elif a=='Jayram':
+    print("Hello.")
+else:
+    print("Cannot identify you.")
+
+# ALL conditions of 'b'
+if b=='10':
+    print("Ohk")
+elif b=='13':
+    print("Teenager ayy")
+elif b=='47':
+    print("You are experienced")
+elif b=='7':
+    print("Smol girl what are you doing here?")
+else:
+    print("None of our members have this age.")
+
+#ALL conditions of 'c'
+if c=='1':
+    print("Are you mehu? Because i don't have brain")
+elif c=='7':
+    print("Hi ojas.")
+elif c=='12':
+    print("Sussy")
+elif c=='I am graduated':
+    print("OH sorry i didn't know.")
+else:
+    print("Sussy member spotted")
+
+
+
+
+
+
+
+
+
+
+
